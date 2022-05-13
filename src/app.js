ca799d679efd40ff15703d6509c6fcd5b700f43d
kag0224 <101907323+kag0224@users.noreply.github.com>
2022-05-12 22:36:27 -0700

    fixed extended forecast display

--- src/app.js
+++ src/app.js
@@ -22,8 +22,7 @@ function formatDate(timestamp) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Fri", "Sat", "Sun", "Mon"];
@@ -49,7 +48,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "748e575d7c9421f3b821d29a87e2a544";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}$units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
